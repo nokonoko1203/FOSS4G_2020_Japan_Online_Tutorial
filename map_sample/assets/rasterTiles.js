5a82a6c86635a7d8fd7b59ea472e15d4d016c628
nokonoko1203 <healtheworld.1203@gmail.com>
2020-11-06 11:29:37 +0900

    add raster tiles url

---
+++ map_sample/assets/rasterTiles.js
@@ -0,0 +1,3 @@
+const stdTiles = 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png'
+const blankTiles = 'https://cyberjapandata.gsi.go.jp/xyz/blank/{z}/{x}/{y}.png'
+const seamlessPhotoTiles = 'https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg'
